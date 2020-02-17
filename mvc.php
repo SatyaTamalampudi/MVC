5c9b2e8512a0deb148fe6448f245e51a3dfc3838
Satya <tetalasatya123@gmail.com>
2020-02-17 17:14:22 +0000

    indented files

--- mvc.php
+++ mvc.php
@@ -1,54 +1,54 @@
-<?php
-class Model
-{
-public $string;
+  <?php
+  class Model
+  {
+  public $string;
 
-public function __construct(){
-  $this->string = "Using PHP in MVC! Inside the Model";
+  public function __construct(){
+    $this->string = "Using PHP in MVC! Inside the Model";
+    }
   }
-}
 
-class View
-{
-  private $model;
-  private $controller;
-	
-  
-  public function __construct($model,$controller){
-    $this->model = $model;
-    $this->controller= $controller;
-   }
-   
-   public function output(){
-      
-     return '<p><a href = "mvc.php?action=clicked">'.$this->model->string."</a><p>";
-      }
-   }
+  class View
+  {
+    private $model;
+    private $controller;
 
- 
- class Controller
-{
-  private $model;
-  
-  public function __construct($model){
-    $this->model = $model;
-    
-   }
-   
-   public function clicked(){
-      
-      $this->model->string = "using the controller to update the view";
-     
-      }
-   }
-   
-   $modelmvc = new model();
-   $controllermvc = new controller($modelmvc);
-   $viewmvc = new View($modelmvc,$controllermvc);
-   echo $viewmvc->output();
 
-   //. is the same as ->
-   
-   
-   
-  ?>
+    public function __construct($model,$controller){
+      $this->model = $model;
+      $this->controller= $controller;
+     }
+
+     public function output(){
+
+       return '<p><a href = "mvc.php?action=clicked">'.$this->model->string."</a><p>";
+        }
+     }
+
+
+   class Controller
+  {
+    private $model;
+
+    public function __construct($model){
+      $this->model = $model;
+
+     }
+
+     public function clicked(){
+
+        $this->model->string = "using the controller to update the view";
+
+        }
+     }
+
+     $modelmvc = new model();
+     $controllermvc = new controller($modelmvc);
+     $viewmvc = new View($modelmvc,$controllermvc);
+     echo $viewmvc->output();
+
+     //. is the same as ->
+
+
+
+    ?>
--- navigation.php
+++ navigation.php
@@ -1,16 +1,16 @@
-<?php
-$mydir = '.';
-$pages = scandir($mydir);
+	<?php
+	$mydir = '.';
+	$pages = scandir($mydir);
 
-echo '<div class="menu">';
+	echo '<div class="menu">';
 
-foreach($pages as $page){
-	if(strpos($page, '.php')){
-		$link = $mydir.'/'.$page;
-		$safeName = str_replace('.php', '', $page);
-		echo '<a href="'.$link.'">'.$safeName.'</a> &nbsp; &nbsp;';
+	foreach($pages as $page){
+		if(strpos($page, '.php')){
+			$link = $mydir.'/'.$page;
+			$safeName = str_replace('.php', '', $page);
+			echo '<a href="'.$link.'">'.$safeName.'</a> &nbsp; &nbsp;';
 
+		}
 	}
-}
-echo '</div>';
-?>
+	echo '</div>';
+	?>
